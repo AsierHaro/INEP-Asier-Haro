e70fb0cc93b3e9ce231b9dbf4d03c83654aff47e
natal <natal@DESKTOP-NNTU6S5>
2024-12-30 16:01:00 +0100

    ahorasi

---
+++ CercadoraPelicula.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "PassarelaVisualitzaPel.h"
+#include "ConnexioBD.h"
+
+class CercadoraPelicula
+{
+public:
+    PassarelaVisualitzaPel cercaVisualitzacions(string titol) {
+        ConnexioBD bd;
+        std::string sql =
+            "SELECT c.titol, DATE_FORMAT(p.data_estrena, '%d/%m/%Y') AS data_formateada, "
+            "p.duracio, c.descripcio, c.qualificacio, "
+            "p.data_estrena <= NOW() AS estrenada "
+            "FROM contingut c "
+            "INNER JOIN pelicula p ON c.titol = p.titol "
+            "WHERE c.titol = '" + titol + "'";
+
+        sql::ResultSet* res = bd.execQuery(sql);
+        PassarelaVisualitzaPel resultat;
+        if (!res->next()) {
+            delete res;
+            throw std::runtime_error("La película '" + titol + "' no existe en la base de datos.");
+        }
+        else {
+            if (!res->getBoolean("estrenada")) {
+                delete res;
+                throw std::runtime_error("La película '" + titol + "' aún no se ha estrenado.");
+            }
+            resultat = PassarelaVisualitzaPel(res->getString("titol"), res->getInt("duracio"), " ", res->getString("data_formateada"), res->getString("descripcio"), 0, res->getString("qualificacio"));
+        }
+        delete res;
+
+
+        return resultat;
+    }
+};
